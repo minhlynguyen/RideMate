146ee058ae68eca2b066284b79794dc8040ce763
yuanindublin <yingyuan.li@ucdconnect.ie>
2023-02-24 15:56:28 +0000

    Update stationAPI.py

--- stationAPI.py
+++ stationAPI.py
@@ -2,6 +2,7 @@
 import requests
 import traceback
 import datetime
+import datetime
 import time
 import json
 from pprint import pprint
@@ -12,10 +13,19 @@ STATIONS_URL="https://api.jcdecaux.com/vls/v1/stations"
 
 
 def write_to_file(text):
+    now = datetime.datetime.now()
     with open("data/bikes_{}".format(now).replace(" ","_"),"w") as f:
         f.write(r.text)
 
-# def write_to_db(text):
+def write_to_db(text):
+    stations=json.loads(text)
+    print(type(stations),len(stations))
+
+    for station in stations:
+        print(station)
+        # vals=(station.get('address'),int(station.get('banking')),station.get('available_bike_stands',int(station.get('bonus')),station.get('contract_name'),station.get('name'),get('contract_name'),station.get('number'),station.get('position').get('lat'),station.get('position').get('lng'),station.get('status'))
+        break
+    return
 
 def main():
     # run forever
@@ -25,8 +35,8 @@ def main():
             r=requests.get(STATIONS_URL,params={"apiKey":APIKEY,"contract":NAME})
             # store(json.loads(r.text))
             # print(r,now)
-            # write_to_file(r.text)
-            # write_to_db(r.text)
+            write_to_file(r.text)
+            write_to_db(r.text)
             # now sleep for 5 minutes
             time.sleep(5)
             # r.encoding='utf-8'
@@ -46,7 +56,21 @@ def main():
             # traceback.print_exception(*exc_info)
             print(traceback.format_exc())
         #     print("Failed")
+            # if engine is None:
     return
 
 if __name__=="__main__":
     main()
+
+
+# def stations_to_db(text):
+#     stations=json.loads(text)
+#     print(type(stations),len(stations))
+
+#     for station in stations:
+#         print(station)
+#         # vals=(station.get('address'),int(station.get('banking')),station.get('available_bike_stands',int(station.get('bonus')),station.get('contract_name'),station.get('name'),get('contract_name'),station.get('number'),station.get('position').get('lat'),station.get('position').get('lng'),station.get('status'))
+#         break
+#     return
+
+# stations_to_db(r.text)
