131b7e212a75b5d834b11ceba2e188aa8dd79f06
minhlynguyen <90552104+minhlynguyen@users.noreply.github.com>
2023-02-27 22:50:48 +0000

    resolve #9

--- weatherAPI.py
+++ weatherAPI.py
@@ -1,13 +1,34 @@
+import sqlalchemy as sqla
+import traceback
+import datetime
 import time
 import requests
 import json
 from pprint import pprint
 
+# Getting station data using Station API
+APIKEY="dc37139a1f5e7dbb1220eb237bc420120b3381f4"
+NAME="Dublin"
+STATIONS_URL="https://api.jcdecaux.com/vls/v1/stations" 
+
+r=requests.get(STATIONS_URL,params={"apiKey":APIKEY,"contract":NAME})
+stations=json.loads(r.text)
+
+# From station position getting the weather information there
+
+# Creating a dictionary of station and its weather data: Station number is the key, weather data is the value
+
+weather_station = {}
+
 WEATHER_URL="https://api.open-meteo.com/v1/forecast"
-LATITUDE = 53.31
-LONGITUDE = -6.22
 
-weather=requests.get(WEATHER_URL,params={"latitude":LATITUDE,"longitude":LONGITUDE,"hourly":"temperature_2m","hourly":"precipitation_probability","hourly":"precipitation","hourly":"weathercode","hourly":"windspeed_10m","current_weather":"true","timeformat":"unixtime"})
-weather_dict=json.loads(weather.text)
-pprint(json.loads(weather.text))
+for station in stations:
+    LATITUDE = station['position']['lat']
+    LONGITUDE = station['position']['lng']
+    weather=requests.get(WEATHER_URL,params={"latitude":LATITUDE,"longitude":LONGITUDE,"hourly":"temperature_2m","hourly":"precipitation_probability","hourly":"precipitation","hourly":"weathercode","hourly":"windspeed_10m","current_weather":"true","timeformat":"unixtime"})
+    weather_dict=json.loads(weather.text)
+    
+    # Filling the weather_station dictionary:
+    weather_station[station['number']]=weather_dict
 
+pprint(weather_station[42])
