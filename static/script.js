a258c4c12521d2a324e7199fc72ff7204037c3c6
minhlynguyen <90552104+minhlynguyen@users.noreply.github.com>
2023-04-13 18:56:40 +0100

    Update weather chart

--- static/script.js
+++ static/script.js
@@ -7,11 +7,3 @@ function closeSide() {
 function closeSideRight(){
     document.getElementById('side-bar-right').style.display = 'none';
 }
-
-//map.addListener('mousemove', function (event) {
-//    var pixelOffset = map.getProjection().fromLatLngToPoint(event.latLng);
-//    pixelOffset.x += 10;
-//    pixelOffset.y -= 10;
-//   var newLatLng = map.getProjection().fromPointToLatLng(pixelOffset);
-//   hoverInfo.setPosition(newLatLng);
-//});
