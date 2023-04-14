e84d0419d57ffd1a2e5b3f38d032d60e6ec854a2
yuanindublin <yingyuan.li@ucdconnect.ie>
2023-04-14 13:34:40 +0100

    Update script.js

--- static/script.js
+++ static/script.js
@@ -6,3 +6,18 @@ function closeSide() {
 function closeSideRight(){
     document.getElementById('side-bar-right').style.display = 'none';
 }
+
+
+function openWeather(evt, weatherStat) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(weatherStat).style.display = "block";
+    evt.currentTarget.className += " active";
+}
